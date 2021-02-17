ef34146c1ed6d0e12b3fdb03a36acf85997aecf1
Dennis <Dennisdenhollander@outlook.com>
2021-02-17 15:39:49 +0100

    Minor fixed for DB

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,19 +1,25 @@
-const { Sequelize } = require('sequelize');
-const Umzug = require('umzug');
-const path = require('path');
+const { Sequelize } = require("sequelize");
+const Umzug = require("umzug");
+const path = require("path");
 
 const connect = async () => {
   try {
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-    });
+    const sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        logging: false,
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+      }
+    );
     await sequelize.authenticate();
     console.info(`Connected to database`);
     return sequelize;
   } catch (error) {
-    console.error('Error connecting to database');
+    console.error("Error connecting to database");
     throw error;
   }
 };
@@ -23,14 +29,14 @@ const migrate = async (sequelize) => {
     const umzug = new Umzug({
       migrations: {
         // indicates the folder containing the migration .js files
-        path: path.join(__dirname, './migrations'),
+        path: path.join(__dirname, "./migrations"),
         // inject sequelize's QueryInterface in the migrations
         params: [sequelize.getQueryInterface()],
       },
       // indicates that the migration data should be store in the database
       // itself through sequelize. The default configuration creates a table
       // named `SequelizeMeta`.
-      storage: 'sequelize',
+      storage: "sequelize",
       storageOptions: {
         sequelize: sequelize,
       },
@@ -38,7 +44,7 @@ const migrate = async (sequelize) => {
 
     await umzug.up();
   } catch (error) {
-    console.error('Error migrating database');
+    console.error("Error migrating database");
     throw error;
   }
 };
