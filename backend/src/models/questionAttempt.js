f8d601e394cde06ccb4b6d9395024cad6c3058de
Kasper <kasperkarelse98@gmail.com>
2021-03-03 00:58:09 +0100

    update models + migration

--- backend/src/models/questionAttempt.js
+++ backend/src/models/questionAttempt.js
@@ -10,25 +10,6 @@ module.exports = (sequelize) =>
     // For a description of the content, see: https://drive.google.com/drive/folders/1qd-xRHG27KmAmLOA47oETB5HDJ7UFTeL
     content: {
       type: DataTypes.JSON,
-    },
-    isCompleted: {
-      type: DataTypes.BOOLEAN,
       allowNull: false,
-    },
-    showFeedback: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    correct: {
-      type: DataTypes.INTEGER,
-    },
-    incorrect: {
-      type: DataTypes.INTEGER,
-    },
-    missed: {
-      type: DataTypes.INTEGER,
-    },
-    timeElapsedSeconds: {
-      type: DataTypes.INTEGER,
     },
   });
