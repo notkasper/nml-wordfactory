3ec632fc335f3749f0737d032ab8e0dca83ef203
Dennis <Dennisdenhollander@outlook.com>
2021-03-03 16:53:16 +0100

    WFT-3 | Put everything in try / catch

--- backend/src/seed.js
+++ backend/src/seed.js
@@ -23,95 +23,95 @@ const seed = async () => {
   await db.TeacherStudent.destroy({ where: {} });
   await db.UserLessonGroup.destroy({ where: {} });
 
-  const { users, lessons } = wordfactoryPreprocessed;
-  const [teacher, ...students] = await db.User.bulkCreate(users);
-  await teacher.addStudents(students);
+  try {
+    const { users, lessons } = wordfactoryPreprocessed;
+    const [teacher, ...students] = await db.User.bulkCreate(users);
+    await teacher.addStudents(students);
 
-  const lessonGroup = await db.LessonGroup.create({
-    id: uuid.v4(),
-    title: 'Superuser course',
-  });
-
-  await teacher.addLessonGroups([lessonGroup]);
-  for (const student of students) {
-    await student.addLessonGroups([lessonGroup]);
-  }
-
-  for (const lesson of lessons) {
-    const createdLesson = await db.Lesson.create({
-      id: lesson.lessonId,
-      groupId: lessonGroup.id,
-      prefix: lesson.lessonPrefix,
-      instruction: lesson.lessonInstruction,
-      index: lesson.lessonIndex,
-      title: lesson.lessonTitle,
+    const lessonGroup = await db.LessonGroup.create({
+      id: uuid.v4(),
+      title: 'Superuser course',
     });
 
-    for (const questionGroup of lesson.questionGroups) {
-      const createdQuestionGroup = await db.QuestionGroup.create({
-        id: questionGroup.questionGroupId,
-        lessonId: createdLesson.id,
-        index: questionGroup.questionGroupIndex,
-        title: questionGroup.questionGroupTitle,
-      });
-
-      for (const question of questionGroup.questions) {
-        await db.Question.create({
-          id: question.questionId,
-          groupId: createdQuestionGroup.id,
-          data: question.data,
-          index: question.questionIndex,
-          type: question.type,
-          instruction: question.instruction,
-        });
-      }
+    await teacher.addLessonGroups([lessonGroup]);
+    for (const student of students) {
+      await student.addLessonGroups([lessonGroup]);
     }
-  }
 
-  for (const user of users.filter((e) => e.role === 'student')) {
-    const lessonAttempts = user.lessonAttempts;
-    for (const lessonAttempt of lessonAttempts) {
-      const createdLessonAttempt = await db.LessonAttempt.create({
-        id: lessonAttempt.id,
-        userId: user.id,
-        lessonId: lessonAttempt.lessonId,
-        stoppedTime: lessonAttempt.stoppedTime,
-        startedTime: lessonAttempt.startedTime,
-        isStopped: lessonAttempt.isStopped,
-        isStarted: lessonAttempt.isStarted,
-        isCompleted: lessonAttempt.isCompleted,
+    for (const lesson of lessons) {
+      const createdLesson = await db.Lesson.create({
+        id: lesson.lessonId,
+        groupId: lessonGroup.id,
+        prefix: lesson.lessonPrefix,
+        instruction: lesson.lessonInstruction,
+        index: lesson.lessonIndex,
+        title: lesson.lessonTitle,
       });
 
-      for (const questionGroupAttempt of lessonAttempt.questionGroups) {
-        const createdQuestionGroupAttempt = await db.QuestionGroupAttempt.create(
-          {
-            id: questionGroupAttempt.id,
-            lessonAttemptId: createdLessonAttempt.id,
-            questionGroupId: questionGroupAttempt.questionGroupId,
-            timeElapsedSeconds: questionGroupAttempt.timeElapsedSeconds
-              ? Math.round(questionGroupAttempt.timeElapsedSeconds)
-              : 0,
-            correct: questionGroupAttempt.correct,
-            incorrect: questionGroupAttempt.incorrect,
-            missed: questionGroupAttempt.missed,
-            isCompleted: questionGroupAttempt.isCompleted || false,
-            showFeedback: questionGroupAttempt.showFeedback || false,
-          }
-        );
+      for (const questionGroup of lesson.questionGroups) {
+        const createdQuestionGroup = await db.QuestionGroup.create({
+          id: questionGroup.questionGroupId,
+          lessonId: createdLesson.id,
+          index: questionGroup.questionGroupIndex,
+          title: questionGroup.questionGroupTitle,
+        });
 
-        for (const questionAttempt of questionGroupAttempt.answers) {
-          await db.QuestionAttempt.create({
-            id: questionAttempt.id,
-            groupAttemptId: createdQuestionGroupAttempt.id,
-            questionId: questionAttempt.questionId,
-            content: questionAttempt.content,
+        for (const question of questionGroup.questions) {
+          await db.Question.create({
+            id: question.questionId,
+            groupId: createdQuestionGroup.id,
+            data: question.data,
+            index: question.questionIndex,
+            type: question.type,
+            instruction: question.instruction,
           });
         }
       }
     }
-  }
 
-  try {
+    for (const user of users.filter((e) => e.role === 'student')) {
+      const lessonAttempts = user.lessonAttempts;
+      for (const lessonAttempt of lessonAttempts) {
+        const createdLessonAttempt = await db.LessonAttempt.create({
+          id: lessonAttempt.id,
+          userId: user.id,
+          lessonId: lessonAttempt.lessonId,
+          stoppedTime: lessonAttempt.stoppedTime,
+          startedTime: lessonAttempt.startedTime,
+          isStopped: lessonAttempt.isStopped,
+          isStarted: lessonAttempt.isStarted,
+          isCompleted: lessonAttempt.isCompleted,
+        });
+
+        for (const questionGroupAttempt of lessonAttempt.questionGroups) {
+          const createdQuestionGroupAttempt = await db.QuestionGroupAttempt.create(
+            {
+              id: questionGroupAttempt.id,
+              lessonAttemptId: createdLessonAttempt.id,
+              questionGroupId: questionGroupAttempt.questionGroupId,
+              timeElapsedSeconds: questionGroupAttempt.timeElapsedSeconds
+                ? Math.round(questionGroupAttempt.timeElapsedSeconds)
+                : 0,
+              correct: questionGroupAttempt.correct,
+              incorrect: questionGroupAttempt.incorrect,
+              missed: questionGroupAttempt.missed,
+              isCompleted: questionGroupAttempt.isCompleted || false,
+              showFeedback: questionGroupAttempt.showFeedback || false,
+            }
+          );
+
+          for (const questionAttempt of questionGroupAttempt.answers) {
+            await db.QuestionAttempt.create({
+              id: questionAttempt.id,
+              groupAttemptId: createdQuestionGroupAttempt.id,
+              questionId: questionAttempt.questionId,
+              content: questionAttempt.content,
+            });
+          }
+        }
+      }
+    }
+
     console.log('[SEEDING]: Seeding completed');
   } catch (error) {
     console.error(error);
