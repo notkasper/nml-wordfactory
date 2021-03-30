dc74a6275592314cf21271777ae45705be2277c1
Kasper Karelse <kasperkarelse98@gmail.com>
2021-03-30 17:16:54 +0200

    small change in seeding

--- backend/src/seed.js
+++ backend/src/seed.js
@@ -33,7 +33,7 @@ const seed = async () => {
 
     const theClass = await db.Class.create({
       id: 'b5fb3711-1157-4f9b-b387-d9e549561012',
-      name: 'My first class',
+      name: 'Oefen klas 1',
     });
 
     await theClass.addTeacher(teacher);
