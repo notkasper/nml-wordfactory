c57ae48706c9bed42800e3e2436a5f8808ea7391
Kasper Karelse <kasperkarelse98@gmail.com>
2021-03-31 10:27:53 +0200

    wip question data visualisation

--- frontend/src/pages/questionStats/index.jsx
+++ frontend/src/pages/questionStats/index.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import PageContainer from '../_shared/PageContainer';
 import service from '../../service';
+import Doughnut from './Doughnut';
 
 const QuestionStats = (props) => {
   const [questionGroupAttempts, setQuestionGroupAttempts] = useState([]);
@@ -16,7 +21,8 @@ const QuestionStats = (props) => {
     if (!response) {
       return;
     }
-    setQuestionGroupAttempts(response.body.data);
+    const data = response.body.data.filter((attempt) => attempt.isCompleted);
+    setQuestionGroupAttempts(data);
     setLoading(false);
   };
 
@@ -24,8 +30,36 @@ const QuestionStats = (props) => {
     loadQuestionGroupAttempts();
   }, []);
 
+  if (loading) {
+    return <CircularProgress />;
+  }
+
   return (
-    <PageContainer>{questionGroupAttempts.map((e) => e.id)}</PageContainer>
+    <PageContainer>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Doughnut
+            questionGroupAttempts={questionGroupAttempts}
+            title="Correctheid verdeling van de vraag groep"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Paper style={{ padding: '2rem', height: '100%' }}>
+            <Typography>Meer data visualisatie</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper style={{ padding: '2rem', height: '100%' }}>
+            <Typography>Meer data visualisatie</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper style={{ padding: '2rem', height: '100%' }}>
+            <Typography>Meer data visualisatie</Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </PageContainer>
   );
 };
 
