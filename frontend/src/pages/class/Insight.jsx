5125ee3e624840c30f323220b50e2bfda70f755d
Dennis <Dennisdenhollander@outlook.com>
2021-04-21 11:15:07 +0200

    WFT-113 | final

--- frontend/src/pages/class/Insight.jsx
+++ frontend/src/pages/class/Insight.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import { DataGrid } from '@material-ui/data-grid';
 
+import PaperWithHeader from '../_shared/PaperWithHeader';
 import ProgressBar from './ProgressBar';
 
 const useStyles = makeStyles((theme) => ({
@@ -21,33 +21,19 @@ const useStyles = makeStyles((theme) => ({
 		height: 230,
 		padding: theme.spacing(3),
 	},
-	table: {
-		borderBottomLeftRadius: 4,
-		borderBottomRightRadius: 4,
-		borderTopLeftRadius: 0,
-		borderTopRightRadius: 0,
-	},
-	paperheader: {
-		textAlign: 'center',
-		color: 'white',
-		borderBottomLeftRadius: 0,
-		borderBottomRightRadius: 0,
-		borderTopLeftRadius: 4,
-		borderTopRightRadius: 4,
-	}
 }));
 
 const columns = [
   {
     field: 'student',
     headerName: 'Naam',
-    width: 200,
+    flex: 0.65,
     valueGetter: (params) => params.row.name
   },
   {
 		field: 'correctness',
 		headerName: 'Gem. correctheid (%)',
-		width: 200,
+		flex: 0.35,
 		valueGetter: (params) => params.row.correctness
 	}
 ];
@@ -59,62 +45,46 @@ const Insight = () => {
   return (
 		<div>
 			<Grid container spacing={3} className={classes.widget}>
-				<Grid item xs={12} md={6}>
-					<Paper className={classes.paperheader} style={{ backgroundColor: theme.widget.primary.main }}>
-						<Typography>Leerlingen die aandacht nodig hebben</Typography>
-					</Paper>
-					<Paper className={classes.table}>
-						<DataGrid
-							autoHeight
-							rows={[
-								{ id: 1, name: 'Naam 1', correctness: 29 }, 
-								{ id: 2, name: 'Naam 2', correctness: 33 }, 
-								{ id: 3, name: 'Naam 3', correctness: 34 }
-							]}
-							columns={columns}
-							hideFooterPagination={true}
-						/>
-					</Paper>
-				</Grid>
-				<Grid item xs={12} md={6}>
-					<Paper className={classes.paperheader} style={{ backgroundColor: theme.widget.primary.main }}>
-						<Typography>Top 3 leerlingen</Typography>
-					</Paper>
-					<Paper className={classes.table}>
-						<DataGrid
-							autoHeight
-							rows={[
-								{ id: 4, name: 'Naam 1', correctness: 89 }, 
-								{ id: 5, name: 'Naam 2', correctness: 84 }, 
-								{ id: 6, name: 'Naam 3', correctness: 79 }
-							]}
-							columns={columns}
-							hideFooterPagination={true}
-						/>
-					</Paper>
-				</Grid>
+				<PaperWithHeader headercolor={theme.widget.primary.main} headertitle="Leerlingen die aandacht nodig hebben">
+					<DataGrid
+						autoHeight
+						rows={[
+							{ id: 1, name: 'Soledad Considine', correctness: 29 }, 
+							{ id: 2, name: 'Arvid Macejkovic', correctness: 33 }, 
+							{ id: 3, name: 'Allen Kunze', correctness: 34 }
+						]}
+						columns={columns}
+						hideFooterPagination={true}
+					/>
+				</PaperWithHeader>
+				<PaperWithHeader headercolor={theme.widget.primary.main} headertitle="Top 3 leerlingen">
+					<DataGrid
+						autoHeight
+						rows={[
+							{ id: 4, name: 'Cleveland Smitham', correctness: 89 }, 
+							{ id: 5, name: 'Dejah Morissette', correctness: 84 }, 
+							{ id: 6, name: 'Maxine Zboncak', correctness: 79 }
+						]}
+						columns={columns}
+						hideFooterPagination={true}
+					/>
+				</PaperWithHeader>
 			</Grid>
 			<Grid container spacing={3} className={classes.widget}>
-				<Grid item xs={12} md={6}>
-					<Paper className={classes.paperheader} style={{ backgroundColor: theme.widget.secondary.main }}>
-						<Typography>Probleem categorieën</Typography>
-					</Paper>
+				<PaperWithHeader headercolor={theme.widget.secondary.main} headertitle="Probleem categorieën">
 					<Paper className={classes.paper}>
 						<ProgressBar title="1. Herken morfemen in woorden" value={38}/>
 						<ProgressBar title="2. Herken morfemen in een zin" value={48}/>
 						<ProgressBar title="3. Verwisselen morfemen" value={55}/>
 					</Paper>
-				</Grid>
-				<Grid item xs={12} md={6}>
-					<Paper className={classes.paperheader} style={{ backgroundColor: theme.widget.secondary.main }}>
-						<Typography>Top categorieën</Typography>
-					</Paper>
+				</PaperWithHeader>
+				<PaperWithHeader headercolor={theme.widget.secondary.main} headertitle="Top categorieën">
 					<Paper className={classes.paper}>
 						<ProgressBar title="1. Betekenis morfemen" value={97}/>
 						<ProgressBar title="2. Splits morfemen" value={83}/>
 						<ProgressBar title="3. Alternatieve betekenis morfemen" value={74}/>
 					</Paper>
-				</Grid>
+				</PaperWithHeader>
 			</Grid>
 		</div>
   );
