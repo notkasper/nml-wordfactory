cc3be8b22a95b3002dbac43787d33ba434340ae4
Dennis <Dennisdenhollander@outlook.com>
2021-05-18 15:18:56 +0200

    WFT-164 | Added a basic profile layout

---
+++ frontend/src/pages/profile/AccountInformation.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4, 4, 0, 4),
+  },
+  divider: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+  },
+  account: {
+    margin: theme.spacing(4),
+  },
+  label: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  header: {
+    fontWeight: 'bold',
+  },
+}));
+
+const AccountInformation = (props) => {
+  const classes = useStyles();
+
+  // TODO: contains mocked data, need to retrieve this via store
+  return (
+    <Grid container>
+      <Grid item xs={12} md={12} lg={12} className={classes.divider}>
+        <Typography variant="h5" className={classes.header}>
+          Account Informatie
+        </Typography>
+        <Divider />
+        <Grid container className={classes.account}>
+          <Grid item xs={12} md={3} lg={3}>
+            <Typography className={classes.label}>Gebruikersnaam</Typography>
+          </Grid>
+          <Grid item xs={12} md={9} lg={9}>
+            <TextField
+              disabled
+              value={'bertvis1971'}
+              className={classes.fullWidth}
+            />
+          </Grid>
+        </Grid>
+        <Grid container className={classes.account}>
+          <Grid item xs={12} md={3} lg={3}>
+            <Typography className={classes.label}>E-mailadres</Typography>
+          </Grid>
+          <Grid item xs={12} md={9} lg={9}>
+            <TextField
+              disabled
+              value={'b.vis@outlook.com'}
+              className={classes.fullWidth}
+            />
+          </Grid>
+        </Grid>
+        <Grid container className={classes.account}>
+          <Grid item xs={12} md={3} lg={3}>
+            <Typography className={classes.label}>Wachtwoord</Typography>
+          </Grid>
+          <Grid item xs={12} md={9} lg={9}>
+            <TextField
+              disabled
+              type="password"
+              value={'superuser'}
+              className={classes.fullWidth}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AccountInformation;
--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+
 import PageContainer from '../_shared/PageContainer';
+import ProfileHeader from './ProfileHeader';
+import AccountInformation from './AccountInformation';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4, 4, 0, 4),
+  },
+}));
 
 const Profile = (props) => {
+  const classes = useStyles();
+
   return (
     <PageContainer>
-      <Grid item xs={12} md={12} lg={12}>
-        <Paper>
-          <p>Hier komt het profiel</p>
-        </Paper>
-      </Grid>
+      <Paper className={classes.root}>
+        <ProfileHeader />
+        <AccountInformation />
+      </Paper>
     </PageContainer>
   );
 };
