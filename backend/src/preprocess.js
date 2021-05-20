aa5d4248064b51e9767d3ab2d6262cbbd68769de
Dennis <Dennisdenhollander@outlook.com>
2021-05-20 20:49:59 +0200

    WFT-174 | Replace odd names with most common Dutch names

--- backend/src/preprocess.js
+++ backend/src/preprocess.js
@@ -357,7 +357,7 @@ const preprocess = async () => {
   });
 
   fs.writeFileSync(
-    'wordfactory-preprocessed.json',
+    '../wordfactory-preprocessed.json',
     JSON.stringify(preprocessedData)
   );
 };
