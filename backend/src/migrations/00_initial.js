ca024ec35d2e360203358c78fce03330af6476ac
Dennis <Dennisdenhollander@outlook.com>
2021-05-25 18:11:23 +0200

    WFT-193 | Fix migrations

--- backend/src/migrations/00_initial.js
+++ backend/src/migrations/00_initial.js
@@ -214,10 +214,6 @@ const up = async (query) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    contentTags: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
     instruction: {
       type: DataTypes.TEXT,
       allowNull: false,
@@ -312,10 +308,6 @@ const up = async (query) => {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
-    score: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
     isCompleted: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
@@ -347,22 +339,6 @@ const up = async (query) => {
     questionId: {
       type: DataTypes.UUID,
       allowNull: false,
-    },
-    correct: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    incorrect: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    missed: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    score: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
     },
     content: {
       type: DataTypes.JSON,
