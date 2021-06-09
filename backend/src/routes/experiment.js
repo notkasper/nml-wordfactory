b6b442c3c810608a66dbccfd91e0a690a900fde4
Dennis <Dennisdenhollander@outlook.com>
2021-06-09 16:16:02 +0200

    WFT-196 | Remove all experiment files

--- backend/src/routes/experiment.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { main } = require('../controllers/experiment');
-
-const router = express.Router();
-
-router.route('/:stage').post(main);
-
-module.exports = router;
